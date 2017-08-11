b7773fc1c0e13ddf06438538dbad3636ffb6873b
ken <ken new@ken-no-mac-pro-3.local>
2017-08-11 14:29:08 +0900

    2nd version

---
+++ SlideShowApp/LargeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LargeViewController.swift
+//  SlideShowApp
+//
+//  Created by ken on 2017/07/30.
+//  Copyright © 2017年 ken. All rights reserved.
+//
+
+import UIKit
+
+class LargeViewController: UIViewController {
+    
+    var number: Int?
+
+    @IBOutlet weak var largeView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        largeView.contentMode = UIViewContentMode.scaleAspectFit
+        largeView.image = UIImage(named: "\(number!).jpg")
+        
+        largeView.contentMode = UIViewContentMode.scaleAspectFit
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -10,16 +10,125 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    var imageArray:[UIImage] = []   //画像の配列
+    var photoNumber:Int = 0         //画像の番号
+    var slideShow = false           //スライドショーが再生中か否か
+    //var maxNumber:Int = 0         //写真の枚数
+    var timer:Timer!
+    var timer_sec = 0
+
+    //画像を表示する関数
+    func showPicture(){
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.image = imageArray[photoNumber]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //画像を配列に保存
+        for i in 0...2{
+            imageArray.append(UIImage(named: "\(i).jpg")!)
+         }
+        
+        /*
+        while let image = UIImage(named: "(imageArray.count).jpg") {
+            imageArray.append(image)
+            maxNumber += 1
+        }
+        */
+        
+        showPicture()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //タイマーで画像の番号を加算
+    func updateTimer(timer:Timer){
+        timer_sec += 2
+        if photoNumber < 2 {
+            photoNumber += 1
+        }else{
+            photoNumber = 0}
+        
+        showPicture()
+    }
 
+    //「進む」ボタンのアウトレットとアクション
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBAction func forwardButton(_ sender: UIButton) {
+        if photoNumber < 2 {
+            photoNumber += 1
+        }else{
+            photoNumber = 0}
+            showPicture()
+        }
 
+    //「戻る」ボタンのアウトレットとアクション
+    @IBOutlet weak var backButton: UIButton!
+    @IBAction func backButton(_ sender: UIButton) {
+        if photoNumber > 0 {
+            photoNumber -= 1
+        }else{
+            photoNumber = 2}
+            showPicture()
+        }
+
+    //「再生／停止」ボタンのアウトレットとアクション
+    @IBOutlet weak var playAndStop: UIButton!
+    @IBAction func playAndStop(_ sender: UIButton) {
+        
+        //スライドショー再生中
+        if slideShow == false{
+            sender.setTitle("停止", for: .normal)
+            slideShow = true
+            timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            
+            forwardButton.isEnabled = false
+            forwardButton.alpha = 0.1
+            backButton.isEnabled = false
+            backButton.alpha = 0.1
+        
+        //スライドショー停止中
+        }else{
+            sender.setTitle("再生", for: .normal)
+            timer.invalidate()
+            slideShow = false
+            
+            forwardButton.isEnabled = true
+            forwardButton.alpha = 1.0
+            backButton.isEnabled = true
+            backButton.alpha = 1.0
+        }
+    }
+    
+    //画面遷移の準備＝表示されている画像の番号を遷移先に渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let largeViewController:LargeViewController = segue.destination as! LargeViewController
+        largeViewController.number = photoNumber
+        
+        //スライドショーが再生中ならば停止状態に戻す
+        if slideShow == true{
+            timer.invalidate()
+            slideShow = false
+        
+            forwardButton.isEnabled = true
+            forwardButton.alpha = 1.0
+            backButton.isEnabled = true
+            backButton.alpha = 1.0
+        
+            playAndStop.setTitle("再生", for: .normal)
+        }
+    }
+    
+    //遷移先の画面から戻ってくるための関数
+    @IBAction func unwind(_ segue:UIStoryboardSegue){
+    }
 }
 
